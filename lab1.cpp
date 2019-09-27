df92e6f2c8d96d04f5dd4900da68fb1c53346df4
mandarinus9 <mandarinus9@gmail.com>
2019-09-27 19:00:11 +0300

    full lab commit

---
+++ lab1.cpp
@@ -0,0 +1,48 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <iostream>
+#include <string>
+
+constexpr unsigned max_lenght = 30;
+
+int main(int argc, char** argv)
+{
+	std::string picture;
+	if (argc != 2)
+		picture = "picture.jpg";
+	else
+		picture = argv[1];
+
+
+	cv::Mat image = cv::imread(picture, cv::IMREAD_COLOR);
+
+	if (image.empty())
+	{
+		std::cout << "Some error, couldn't read the file!" << std::endl;
+		return -1;
+	}
+
+	cv::Mat resizedImg;
+	cv::resize(image, resizedImg, cv::Size(), 0.3, 0.3, cv::INTER_AREA);
+	
+	cv::Mat gaussianBlur;
+	cv::GaussianBlur(resizedImg, gaussianBlur, cv::Size(5, 5), 0);
+	
+	for (int i = 51; i > 1; i -= 2)
+	{
+		cv::GaussianBlur(resizedImg, gaussianBlur, cv::Size(i, i), 0, 0);
+
+		cv::imshow("Gaussian BLURRRRR!", gaussianBlur);
+
+		cv::waitKey(10);
+	}
+
+	cv::imshow("Original", resizedImg);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+
+	return 0;
+}
